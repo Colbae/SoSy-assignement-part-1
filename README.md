dae22de8fee76b0fa5d4c040ae895ea93c9d8eb2
Colin Bächtold <colin.baechtold@proton.me>
2025-11-17 18:12:44 +0100

    Extract repository path into an env var and save graphs as files

--- README.md
+++ README.md
@@ -14,6 +14,11 @@
   # source .venv/bin/activate  # macOS/Linux
   pip install -r requirements.txt
   ```
+- **Runing:**
+  ```bash
+  source setup.sh  # macOS/Linux
+  jupyter notebook 
+  ```
 
 ---
 
--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-def plot_network_graph(file_pairs):
+def plot_network_graph(file_pairs, output_filename, fig_size):
     G = nx.Graph()
 
     for file1, file2, count in file_pairs:
         G.add_edge(file1, file2, weight=count)
 
-    plt.figure(figsize=(12, 10))
+    plt.figure(figsize=fig_size)
     pos = nx.circular_layout(G)
     nx.draw(G, pos, with_labels=True, node_size=700, node_color='lightblue', font_size=10, font_weight='bold', edge_color='gray')
 
@@ -15,4 +15,5 @@ def plot_network_graph(file_pairs):
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
 
     plt.title("Logical Coupling Network Graph")
+    plt.savefig(output_filename, dpi=300, bbox_inches='tight')
     plt.show()
